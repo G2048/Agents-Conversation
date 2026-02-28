c99f075f3cd5b682b02874697787629b9e8d72ba
IX <borntokill.ix@gmail.com>
2026-02-28 18:46:33 +0700

    feat(app/agent/utils/state.py): add ConversationState.EOF StrEnum; rename AgentState.save_point to AgentState.eof

--- app/agent/utils/state.py
+++ app/agent/utils/state.py
@@ -1,3 +1,4 @@
+from enum import StrEnum
 from typing import Annotated
 
 from langgraph.graph import add_messages
@@ -5,8 +6,12 @@ from langgraph.graph.message import Messages
 from pydantic import BaseModel
 
 
+class ConversationState(StrEnum):
+    EOF = "DIXI"
+
+
 class AgentState(BaseModel):
     messages: Annotated[Messages, add_messages]
     current_tool: str | None = None
     iteration_counter: int = 0
-    save_point: bool = False
+    eof: bool = False
